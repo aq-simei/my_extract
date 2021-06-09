7eb2107aac24c0d240710837a16327126150a542
Antonio Simei <aq.simei@gmail.com>
2021-06-09 16:20:48 -0300

    Added Styled-Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import styled from 'styled-components'
+//Ao utilizar styled components, as propriedades são dadas a um componente (letra maiúscula e aspas duplas `estilo`)
+const Title = styled.h1`
+`
 export function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Title>Hello World</Title>
     </div>
-  );
+  ); //ao usar styled-components, a tag html muda: styled.h1 -> tag no código html recebe o nome do componente, nesse caso, Title
 }
